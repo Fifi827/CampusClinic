a650e4d7ab99cf42c321c0f3a7e873c08ac23c79
Hloni <vkheleli@gmail.com>
2020-11-24 17:25:16 +0200

    innitial commit

---
+++ app/src/main/java/com/cut/campusclinic/User.java
@@ -0,0 +1,9 @@
+package com.cut.campusclinic;
+
+public class User
+{
+    private String userID;
+    private String userFirstName;
+    private String userLastName;
+    private String userRole;
+}
